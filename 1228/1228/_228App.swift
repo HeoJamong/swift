3a243ee0887d418bba497f7ea853001ffe7819f0
Heojamong <wlaud0823@naver.com>
2023-01-09 21:36:23 +0900

    Initial Commit

---
+++ 1228/1228/_228App.swift
@@ -0,0 +1,17 @@
+//
+//  _228App.swift
+//  1228
+//
+//  Created by 허지명 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct _228App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
